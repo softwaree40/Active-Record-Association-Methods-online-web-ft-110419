0294fe6a393c4bfa7e40b2fb5ce898b1cc8819b8
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-31 05:55:18 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :genres, through: :songs
+  has_many :genres, :through => :songs
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
